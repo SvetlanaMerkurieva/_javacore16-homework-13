0c135b9aa0f887a6dc9b7af6171187f6162ee036
SvetlanaMerkurieva <82316953+SvetlanaMerkurieva@users.noreply.github.com>
2023-12-07 20:21:50 +0200

    Adds task 1 parts 1, 2, 3

--- src/main/java/task1/UsersTests.java
+++ src/main/java/task1/UsersTests.java
@@ -4,19 +4,26 @@ import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.gson.reflect.TypeToken;
 
+import org.jsoup.Connection;
 import org.jsoup.Jsoup;
 
 import task1.dto.User;
 import task1.dto.UserAddress;
 import task1.dto.UserAddressGeo;
 import task1.dto.UserCompany;
+
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
 import java.lang.reflect.Type;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.List;
 import java.util.Scanner;
 
 public class UsersTests {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException, InterruptedException {
         String urlAllUsers = "https://jsonplaceholder.typicode.com/users";
         String allUsers = Jsoup.connect(urlAllUsers).ignoreContentType(true).get().body().text();
         Type typeToken = TypeToken.getParameterized(List.class, User.class).getType();
@@ -36,12 +43,83 @@ public class UsersTests {
         UserAddress newUserAddress = new UserAddress("О. Поля", "дім 44", "Дніпро", "49000", newUserAddressGeo);
         int newId = users.size()+1;
         User newUser = new User(newId, "Lana Merkury", "Merlana", "merlana@gmail.com" , newUserAddress, "093 193 44 85", "south.com.ua", newUserCompany);
-        System.out.println("Кількість користувачів: " + users.size());
         users.add(newUser);
         Gson gson = new GsonBuilder().setPrettyPrinting().create();
-        String jsonUser = gson.toJson(newUser);
-        System.out.println("Новий створенний користувач: " + jsonUser);
-        System.out.println("Кількість користувачів після створення нового користувача: " + users.size());
+        String jsonUsers = gson.toJson(users);
+
+        URL url = new URL("https://jsonplaceholder.typicode.com/users/");
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+        connection.setRequestMethod("POST");
+        connection.setDoOutput(true);
+        OutputStream os = connection.getOutputStream();
+        os.write(jsonUsers.getBytes());
+        os.flush();
+        os.close();
+
+        int responseCode = connection.getResponseCode();
+        System.out.println("POST responseCode: " + responseCode);
+        StringBuffer response = new StringBuffer();
+
+        if (responseCode == HttpURLConnection.HTTP_CREATED) {
+            BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+            String inputLine;
+             while ((inputLine = in.readLine()) != null) {
+                 response.append(inputLine);
+             }
+             in.close();
+        } else {
+            System.out.println("POST request not worked");
+        }
+        System.out.println(response);
+
+        System.out.println("------------------------------------------------------------------");
+
+        System.out.println("Зміна даних користувача з id 5:");
+        User userId5 = users.get(4);
+
+        userId5.setUsername("Trino");
+        userId5.setPhone("***-***-**-**");
+
+        String jsonUserId5 = gson.toJson(userId5);
+
+        URL urlPut = new URL("https://jsonplaceholder.typicode.com/users/5");
+        HttpURLConnection connectionPut = (HttpURLConnection) urlPut.openConnection();
+        connectionPut.setDoOutput(true);
+        connectionPut.setRequestMethod("PUT");
+
+        OutputStream osP = connectionPut.getOutputStream();
+        osP.write(jsonUserId5.getBytes());
+        osP.flush();
+        osP.close();
+
+        int responseCodePut = connectionPut.getResponseCode();
+        System.out.println("PUT responseCode: " + responseCodePut);
+
+        StringBuffer responsePut = new StringBuffer();
+
+        if (responseCodePut== HttpURLConnection.HTTP_OK) {
+            BufferedReader in = new BufferedReader(new InputStreamReader(connectionPut.getInputStream()));
+            String inputLine;
+            while ((inputLine = in.readLine()) != null) {
+                responsePut.append(inputLine);
+            }
+            in.close();
+        } else {
+            System.out.println("PUT request not worked");
+        }
+        System.out.println(responsePut);
+
+        System.out.println("------------------------------------------------------------------");
+
+        System.out.println("Видалення користувача з id 4:");
+        URL urlUserDel = new URL("https://jsonplaceholder.typicode.com/users/4");
+
+        HttpURLConnection connectionDel = (HttpURLConnection) urlUserDel.openConnection();
+        connectionDel.setRequestMethod("DELETE");
+        connectionDel.setDoOutput(true);
+
+        int responseCodeDel = connectionDel.getResponseCode();
+        System.out.println("DELETE responseCode: " + responseCodeDel);
 
         System.out.println("------------------------------------------------------------------");
 
@@ -63,6 +141,7 @@ public class UsersTests {
         System.out.println(userByUsername);
 
         System.out.println("------------------------------------------------------------------");
+        Jsoup.connect(urlAllUsers).method(Connection.Method.PUT);
 
     }
 
